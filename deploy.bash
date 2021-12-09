2d4d18e82f4a1a35f7dc4a6e70e1cefce0f2b54b
FanWeiJie <fank243@163.com>
2021-12-09 16:52:50 +0800

    编写部署脚本

--- deploy.bash
+++ deploy.bash
@@ -10,20 +10,24 @@ version=`date +%m%d%H`
 for serve in "${project[@]}"
 do
 
-echo "清理镜像及容器"
+echo "******************************** 操作容器[${serve}]开始 ***************************"
 container=${serve}
-image="registry.cn-shanghai.aliyuncs.com/${container}"
+image="registry.cn-shanghai.aliyuncs.com/fank243/${container}"
 
-if docker ps | grep "${container}" ;then
-    docker stop "${container}"
+#if docker ps | grep "${container}" ;then
+#    echo "开始停止容器：docker stop ${container}"
+#    docker stop "${container}"
+#fi
+
+if docker ps -a | grep "${container}" ;then
+    echo "开始清理容器：docker rm -f ${container}"
+    docker rm -f "${container}"
 fi
 
-if docker ps -a | grep "${container}";then
-    docker rm "${container}"
-fi
-
-if docker images | grep "${image}";then
-    docker rmi "${image}"
+if docker images | grep "${image}" ;then
+    echo "开始清理镜像：docker rmi $(docker images | grep "${image}*" | awk '{print $3}')"
+    # shellcheck disable=SC2046
+    docker rmi $(docker images | grep "${image}*" | awk '{print $3}')
 fi
 
 case ${serve} in
@@ -34,13 +38,25 @@ server-system)
   port=8903
   ;;
 esac
+echo "端口号设置完成：${port}"
 
-echo "镜像制作"
-docker build -t "${image}:${version}" "${serve}/target/${serve}.jar"
+echo "开始镜像制作：docker build -t ${image}:${version} ${serve}"
+# docker build -t Dockerfile文件所在目录
+docker build -t "${image}:${version}" "${serve}"
 
-echo "推送镜像"
-docker push "${image}"
+echo "开始运行容器：docker run -d --restart=always --name ${container} -p ${port}:${port} ${image}:${version}"
+docker run -d --restart=always --name "${container}" -p ${port}:${port} "${image}:${version}"
 
-echo "容器运行"
-docker run -d --restart=always --name "${container}" -p ${port}:${port}  "${image}" -Duser.timezone=GMT+8
+if [ "$1" == "prod" ]; then
+    echo "开始推送镜像：docker push ${image}:${version}"
+    docker push "${image}:${version}"
+fi
+
+echo "******************************** 操作容器[${serve}]结束 ***************************"
 done
+
+# shellcheck disable=SC2022
+echo "开始清理none镜像：docker rmi $(docker images | grep "none*" | awk '{print $3}')"
+# shellcheck disable=SC2046
+# shellcheck disable=SC2022
+docker rmi $(docker images | grep "none*" | awk '{print $3}')
