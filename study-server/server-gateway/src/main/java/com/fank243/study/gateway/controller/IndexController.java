e813845847e8fe2b0e7d44c04b5e424037fa6790
FanWeiJie <fank243@163.com>
2022-02-15 14:01:42 +0800

    添加robots.txt

---
+++ study-server/server-gateway/src/main/java/com/fank243/study/gateway/controller/IndexController.java
@@ -0,0 +1,21 @@
+package com.fank243.study.gateway.controller;
+
+import com.fank243.study.common.utils.ResultInfo;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 首页控制器
+ * 
+ * @author FanWeiJie
+ * @since 2022-02-15 13:54:32
+ */
+@RequestMapping("")
+@RestController
+public class IndexController {
+
+    @RequestMapping("/")
+    public ResultInfo<?> index() {
+        return ResultInfo.err404();
+    }
+}
